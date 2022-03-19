cfac6a06807bef82b0639c148aca0dcdd824b3fd
nicola-19010 <n.pacheco02@ufromail.cl>
2022-03-18 21:38:28 -0300

    commit nicolas

--- src/main/java/probandogithub1.java
+++ src/main/java/probandogithub1.java
@@ -2,5 +2,6 @@ public class probandogithub1 {
     public static void main(String[] args) {
 
         System.out.println("Texto por patalla");
+        System.out.println("Soy Nicolas Pacheco");
     }
 }
